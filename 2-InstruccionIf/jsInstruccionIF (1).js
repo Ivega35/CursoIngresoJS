cf1eaabf652a9fbca02cdafb6c669f5bbe8ebac0
Ivega35 <52429605+Ivega35@users.noreply.github.com>
2019-07-15 22:25:08 -0300

    Add files via upload

--- 2-InstruccionIf/jsInstruccionIF (1).js
+++ 2-InstruccionIf/jsInstruccionIF (1).js
@@ -1,7 +1,10 @@
 function mostrar()
-{
-//tomo la edad  
-
-
+{   
+//tomo la edad 
+    var edad=parseInt(document.getElementById("edad").value);
+    if(edad == 15){
+        
+    alert("ñiña bonita");
+}
 
 }//FIN DE LA FUNCIÓN
--- 2-InstruccionIf/jsInstruccionIF (6).js
+++ 2-InstruccionIf/jsInstruccionIF (6).js
@@ -1,6 +1,17 @@
 function mostrar()
 {
-//tomo la edad  
+    var edad=parseInt(document.getElementById("edad").value);
+   
+    if(edad >= 13 && edad <=17){
+        alert("Es adolescente")
+    }
+    else if(edad>=18){
+        alert("Es mayor de edad");
+    }
+
+    }
+
+    
 
 
 
