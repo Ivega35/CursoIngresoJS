96e2a16e322a5d46f6a6e499fb69662b87b3b200
Ivega35 <52429605+Ivega35@users.noreply.github.com>
2019-08-14 11:50:36 -0300

    Add files via upload

--- 9-Parciales/2018_1Cuatri_Parcial_fuentes/nueve.js
+++ 9-Parciales/2018_1Cuatri_Parcial_fuentes/nueve.js
@@ -1,4 +1,62 @@
-function mostrar()
-{
+function mostrar() {
+    var marca;
+    var peso;
+    var temperatura;
+    var respuesta;
+    var tempPar = 0;
+    var flag = 0;
+    var pesoMax;
+    var marcaMax;
+    var bajoCero = 0;
+    var acumPeso = 0;
+    var promedio;
+    var pesoMin;
+    var contador = 0
+
+    do {
+        marca = prompt("Ingrese el nombre de la marca:")
+        
+        peso = parseInt(prompt("Ingrese el peso del producto:"))
+
+        while (isNaN(peso) || peso < 1 || peso > 100) {
+            peso = parseInt(prompt("El peso ingresado no es valido, vuelva a ingresar el peso:"))
+        }
+        
+        temperatura = parseInt(prompt("Ingrese la temperatura de almacenamiento:"))
+    
+        while (temperatura < -30 || temperatura > 30) {
+            temperatura = parseInt(prompt("Temperatura no valida. Ingrese la temperatura de almacenamiento nuevamente:"))
+        }
+
+        if (temperatura % 2 == 0) {
+            tempPar++;
+        }
+        if (flag == 0 || peso > pesoMax) {
+            pesoMax = peso;
+            marcaMax = marca;
+
+        }
+        if (flag == 0 || peso < pesoMin) {
+            pesoMin = peso;
+            flag = 1;
+        }
+        if (temperatura < 0) {
+            bajoCero++;
+        }
+        acumPeso = acumPeso + peso;
+        contador++;
+
+
+
+
+        respuesta = prompt("¿Desea seguir ingresando datos?");
+    } while (respuesta == "si")
+    promedio = acumPeso / contador;
+
+    document.write("La cantidad de temperaturas pares es " + tempPar + "<br>");
+    document.write("La marca del producto mas pesado es " + marcaMax + "<br>");
+    document.write("La cantidad de productos conservados bajo cero es " + bajoCero + "<br>");
+    document.write("El promedio del peso de los productos es " + promedio + "<br>");
+    document.write("El peso maximo es  " + pesoMax + " y el peso minimo es " + pesoMin + "<br>");
 
 }
--- 9-Parciales/2018_1Cuatri_Parcial_fuentes/ocho.js
+++ 9-Parciales/2018_1Cuatri_Parcial_fuentes/ocho.js
@@ -1,4 +1,69 @@
-function mostrar()
-{
+function mostrar() {
+    var letra;
+    var numero;
+    var respuesta;
+    var pares = 0;
+    var impares = 0;
+    var resto;
+    var ceros = 0
+    var pos = 0;
+    var sumNeg = 0;
+    var promedio;
+    var contPos = 0;
+    var flag = 0;
+    var maximo;
+    var minimo;
+    var letMax;
+    var letMin;
+
+    do {
+        letra = prompt("Ingrese una letra:");
+        while (!isNaN(letra)) {
+            letra = prompt("El dato ingresado no es una letra, ingrese una letra:");
+        }
+        numero = parseInt(prompt("ingrese un numero:"));
+        while (isNaN(numero) || numero < -100 || numero > 100) {
+            numero = parseInt(prompt("el numero ingresado no es valido, vuelva a ingresar el numero:"));
+        }
+        resto = numero % 2;
+        if (resto == 0 && numero != 0) {
+            pares++;
+        }
+        else if (resto != 0 && numero != 0) {
+            impares++;
+        }
+        else {
+            ceros++;
+        }
+        if (numero >= 0) {
+            pos = pos + numero;
+            contPos++
+        }
+        else {
+            sumNeg = sumNeg + numero;
+        }
+        if (flag == 0 || numero > maximo) {
+            maximo = numero;
+            letMax = letra;
+        }
+        if (flag == 0 || numero < minimo) {
+            minimo = numero;
+            letMin = letra;
+            flag = 1;
+        }
+
+
+        respuesta = prompt("¿Desea seguir ingresando numeros?")
+
+    } while (respuesta == "si")
+    promedio = pos / contPos
+
+    document.write("la cantidad de numeros pares es " + pares + "<br>");
+    document.write("La cantidad de numeros impares es " + impares + "<br>");
+    document.write("La cantidad de ceros es " + ceros + "<br>");
+    document.write("El promedio de todos los numeros positivos es " + promedio + "<br>");
+    document.write("La suma de todos los numeros negativos es " + sumNeg + "<br>");
+    document.write("el numero maximo es " + maximo + " y su letra asociada es " + letMax + "<br>");
+    document.write("El numero minimo es " + minimo + " y su letra asociada es " + letMin + "<br>");
 
 }
